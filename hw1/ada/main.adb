98883907c00c6a01786ad230c042f7e7f0ffeebc
lostcache <harshdevisha9111@gmail.com>
2025-06-09 04:41:11 +0000

    fixes flawed array based impl

--- hw1/ada/main.adb
+++ hw1/ada/main.adb
@@ -4,62 +4,92 @@ with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 procedure Main is
 
    Upper_Bound     : constant Positive := 100;
-   Max_Threads     : constant Positive := 50;
-   First_Thread_Id : constant Positive := 2;
+   Max_Primes      : constant Positive := 25;
 
    task type Thread_Task is
-      entry Start (Id     : in Positive);
+      entry Start (Prime_Number : in Positive);
       entry Put   (Number : in Natural);
    end Thread_Task;
 
    type Thread_Task_Access is access Thread_Task;
 
-   type Thread_Task_Arr is array (Positive range <>) of Thread_Task_Access;
+   type Thread_List_Node;
+   type Thread_List_Access is access Thread_List_Node;
+
+   type Thread_List_Node is record
+      Thread_Ref : Thread_Task_Access;
+      Prime_Val  : Positive;
+      Next_Node  : Thread_List_Access;
+   end record;
 
    protected type Task_Manager is
-      procedure Get_Next_Thread
-        (Current_Id : in Positive; Next : out Thread_Task_Access;
-         Should_Start : out Boolean; Start_Id : out Positive);
+      procedure Add_Thread (Prime_Number : in Positive; Thread_Ref : in Thread_Task_Access);
+      procedure Get_Thread_For_Prime (Prime_Number : in Positive; Thread_Ref : out Thread_Task_Access; Found : out Boolean);
+      procedure Get_Last_Thread (Thread_Ref : out Thread_Task_Access);
+      procedure Create_New_Thread (Prime_Number : in Positive; New_Thread : out Thread_Task_Access);
    private
-      Filters : Thread_Task_Arr(First_Thread_Id .. Max_Threads) := (others => null);
+      Thread_List_Head : Thread_List_Access := null;
+      Thread_List_Tail : Thread_List_Access := null;
    end Task_Manager;
 
    Manager : Task_Manager;
 
    protected body Task_Manager is
-      procedure Get_Next_Thread
-        (Current_Id : in Positive; Next : out Thread_Task_Access;
-         Should_Start : out Boolean; Start_Id : out Positive)
-      is
-         Next_Id : constant Positive := Current_Id + 1;
+      procedure Add_Thread (Prime_Number : in Positive; Thread_Ref : in Thread_Task_Access) is
+         New_Node : Thread_List_Access := new Thread_List_Node'(
+            Thread_Ref => Thread_Ref,
+            Prime_Val  => Prime_Number,
+            Next_Node  => null
+         );
       begin
-         if Next_Id > Max_Threads then
-            Next := null;
-            Should_Start := False;
-            Start_Id := 1;
-            return;
-         end if;
-
-         if Filters(Next_Id) = null then
-            Filters(Next_Id) := new Thread_Task;
-            Should_Start := True;
-            Start_Id := Next_Id;
+         if Thread_List_Head = null then
+            Thread_List_Head := New_Node;
+            Thread_List_Tail := New_Node;
          else
-            Should_Start := False;
-            Start_Id := 1;
+            Thread_List_Tail.Next_Node := New_Node;
+            Thread_List_Tail := New_Node;
          end if;
+      end Add_Thread;
 
-         Next := Filters(Next_Id);
-      end Get_Next_Thread;
+      procedure Get_Thread_For_Prime (Prime_Number : in Positive; Thread_Ref : out Thread_Task_Access; Found : out Boolean) is
+         Current : Thread_List_Access := Thread_List_Head;
+      begin
+         Found := False;
+         Thread_Ref := null;
+
+         while Current /= null loop
+            if Current.Prime_Val = Prime_Number then
+               Thread_Ref := Current.Thread_Ref;
+               Found := True;
+               return;
+            end if;
+            Current := Current.Next_Node;
+         end loop;
+      end Get_Thread_For_Prime;
+
+      procedure Get_Last_Thread (Thread_Ref : out Thread_Task_Access) is
+      begin
+         if Thread_List_Tail /= null then
+            Thread_Ref := Thread_List_Tail.Thread_Ref;
+         else
+            Thread_Ref := null;
+         end if;
+      end Get_Last_Thread;
+
+      procedure Create_New_Thread (Prime_Number : in Positive; New_Thread : out Thread_Task_Access) is
+      begin
+         New_Thread := new Thread_Task;
+         Add_Thread(Prime_Number, New_Thread);
+      end Create_New_Thread;
    end Task_Manager;
 
    task body Thread_Task is
-      My_Id         : Positive;
+      My_Prime      : Positive;
       Next_Thread   : Thread_Task_Access;
       Should_Stop   : Boolean := False;
    begin
-      accept Start (Id : in Positive) do
-         My_Id := Id;
+      accept Start (Prime_Number : in Positive) do
+         My_Prime := Prime_Number;
       end Start;
 
       loop
@@ -69,60 +99,34 @@ procedure Main is
             if Number = 0 then
                if Next_Thread /= null then
                   Next_Thread.Put(0);
-                  -- Put_Line("Thread" & My_Id'Image & " relaying shutdown signal.");
                end if;
                Should_Stop := True;
-            elsif Number mod My_Id /= 0 then
+            elsif Number mod My_Prime /= 0 then
                if Next_Thread = null then
-                  declare
-                     Should_Start : Boolean;
-                     Start_Id : Positive;
-                  begin
-                     Manager.Get_Next_Thread(Number, Next_Thread, Should_Start, Start_Id);
-                     if Should_Start then
-                        Put_Line (Natural'Image(Number));
-                        Next_Thread.Start(Number);
-                     end if;
-                  end;
+                  Put_Line(Natural'Image(Number));
+
+                  Manager.Create_New_Thread(Number, Next_Thread);
+                  Next_Thread.Start(Number);
                end if;
 
                if Next_Thread /= null then
                   Next_Thread.Put(Number);
-               else
-                  -- Put_Line("Thread" & My_Id'Image & " passed " & Natural'Image(Number) & " to end of pipe (prime).");
-                  Put_Line("");
                end if;
-            else
-               -- Put_Line("Thread" & My_Id'Image & " consumed: " & Natural'Image(Number));
-               Put_Line("");
             end if;
          end Put;
       end loop;
-
-      -- Put_Line("Thread" & My_Id'Image & " shutting down.");
    end Thread_Task;
 
    First_Thread : Thread_Task_Access;
 
 begin
    Put_Line("--- START ---");
-   -- Put_Line("Feeding numbers from 2 to" & Positive'Image(Upper_Bound));
 
-   declare
-      Should_Start : Boolean;
-      Start_Id : Positive;
-   begin
-      Manager.Get_Next_Thread(Current_Id   => First_Thread_Id - 1,
-                              Next         => First_Thread,
-                              Should_Start => Should_Start,
-                              Start_Id     => Start_Id);
-      if Should_Start then
-         -- Put_Line ("Spawning thread for ID: " & Positive'Image(Start_Id));
-         First_Thread.Start(Start_Id);
-      end if;
-   end;
+   Manager.Create_New_Thread(2, First_Thread);
+   First_Thread.Start(2);
+   Put_Line(" 2");
 
-   for I in 2 .. Upper_Bound loop
+   for I in 3 .. Upper_Bound loop
       First_Thread.Put(I);
    end loop;
 
